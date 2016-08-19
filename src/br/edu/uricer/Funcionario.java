e36407e5f95a81b64d5c7879cdb9e4edc1822606
Renan Sczcepanik <renan.scz@hotmail.com>
2016-08-19 14:24:19 -0300

    Definidas as classes, o teste e iniciei algumas variáveis

---
+++ src/br/edu/uricer/Funcionario.java
@@ -0,0 +1,8 @@
+package br.edu.uricer;
+
+public class Funcionario {
+    private final String NOME = "";
+    private final String E_MAIL = "";
+    private final double SALARIO_BASE = 0;
+    private final String CARGO = "";
+}
---
+++ test/br/edu/uricer/CalculaSalarioTest.java
@@ -0,0 +1,24 @@
+package br.edu.uricer;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Computer
+ */
+public class CalculaSalarioTest {
+    
+    public CalculaSalarioTest() {
+    }
+        
+    @Test
+    public void testSomeMethod() {
+        
+    }
+    
+}
