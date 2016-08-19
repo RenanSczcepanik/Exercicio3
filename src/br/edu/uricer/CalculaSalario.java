d1bc1f7a44e4b3f069112c049e375e840de7049c
Renan Sczcepanik <renan.scz@hotmail.com>
2016-08-19 16:16:42 -0300

    Criei mais alguns metodos

--- src/br/edu/uricer/CalculaSalario.java
+++ src/br/edu/uricer/CalculaSalario.java
@@ -5,10 +5,26 @@
  */
 package br.edu.uricer;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author Computer
  */
 public class CalculaSalario {
+    private final BigDecimal DESCONTO_DE_25 = new BigDecimal(0.25);
+    private final BigDecimal DESCONTO_DE_20 = new BigDecimal(0.2);
+    private final BigDecimal DESCONTO_DE_15 = new BigDecimal(0.15);
+    private final BigDecimal DESCONTO_DE_10 = new BigDecimal(0.1);
+    public BigDecimal salario;
     
+    public BigDecimal recebeCargo(String cargo){
+        if(cargo.equals("DESENVOLVEDOR")){
+            return calculaDesenvolvedor(new BigDecimal(3000));
+        }
+    }
+    private BigDecimal calculaDesenvolvedor(BigDecimal SALARIO_BASE){
+        this.salario = SALARIO_BASE;
+        return salario.multiply(DESCONTO_DE_25);
+    }
 }
--- src/br/edu/uricer/Funcionario.java
+++ src/br/edu/uricer/Funcionario.java
@@ -1,8 +1,47 @@
 package br.edu.uricer;
 
+import java.math.BigDecimal;
+
 public class Funcionario {
-    private final String NOME = "";
-    private final String E_MAIL = "";
-    private final double SALARIO_BASE = 0;
-    private final String CARGO = "";
+    private String NOME;
+    private String E_MAIL;
+    private BigDecimal SALARIO_BASE;
+    private String CARGO;
+    
+    public Funcionario() {
+    }
+    
+    public String getNOME() {
+        return NOME;
+    }
+
+    public void setNOME(String NOME) {
+        this.NOME = NOME;
+    }
+
+    public String getE_MAIL() {
+        return E_MAIL;
+    }
+
+    public void setE_MAIL(String E_MAIL) {
+        this.E_MAIL = E_MAIL;
+    }
+
+    public BigDecimal getSALARIO_BASE() {
+        return SALARIO_BASE;
+    }
+
+    public void setSALARIO_BASE(BigDecimal SALARIO_BASE) {
+        this.SALARIO_BASE = SALARIO_BASE;
+    }
+
+    public String getCARGO() {
+        return CARGO;
+    }
+
+    public void setCARGO(String CARGO) {
+        this.CARGO = CARGO;
+    }
+    
+    
 }
