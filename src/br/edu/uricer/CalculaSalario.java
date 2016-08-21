4fb6404e018e34e435ddcb7f4831e2f569ed824a
Renan Sczcepanik <renan.scz@hotmail.com>
2016-08-21 18:27:24 -0300

    Melhorias no Sistema de calculo

--- src/br/edu/uricer/CalculaSalario.java
+++ src/br/edu/uricer/CalculaSalario.java
@@ -1,30 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.uricer;
 
 import java.math.BigDecimal;
 
-/**
- *
- * @author Computer
- */
 public class CalculaSalario {
-    private final BigDecimal DESCONTO_DE_25 = new BigDecimal(0.25);
-    private final BigDecimal DESCONTO_DE_20 = new BigDecimal(0.2);
-    private final BigDecimal DESCONTO_DE_15 = new BigDecimal(0.15);
-    private final BigDecimal DESCONTO_DE_10 = new BigDecimal(0.1);
-    public BigDecimal salario;
+
+    private final BigDecimal DESCONTO_DE_30 = new BigDecimal("0.30");
+    private final BigDecimal DESCONTO_DE_25 = new BigDecimal("0.25");
+    private final BigDecimal DESCONTO_DE_20 = new BigDecimal("0.2");
+    private final BigDecimal DESCONTO_DE_15 = new BigDecimal("0.15");
+    private final BigDecimal DESCONTO_DE_10 = new BigDecimal("0.1");
+    private final BigDecimal SALARIO_BASE_DESENVOLVEDOR = new BigDecimal("3000.0");
+    private final BigDecimal SALARIO_BASE_GERENTE = new BigDecimal("5000.0");
+    private final BigDecimal SALARIO_BASE_DBA = new BigDecimal("2000.0");
+    private final BigDecimal SALARIO_BASE_TESTADOR = new BigDecimal("2000.0");
     
-    public BigDecimal recebeCargo(String cargo){
-        if(cargo.equals("DESENVOLVEDOR")){
-            return calculaDesenvolvedor(new BigDecimal(3000));
+    public BigDecimal salario;
+    String cargo;
+
+    public CalculaSalario(BigDecimal salario, String cargo) {
+        this.salario = salario;
+        this.cargo = cargo;
+    }
+
+    public BigDecimal usaCargo() {
+        switch (cargo) {
+            case "DESENVOLVEDOR":
+                return calculaDesenvolvedor();
+            case "GERENTE":
+                return calculaGerente();
+            case "DBA":
+                return calculaDBA();
+            case "TESTADOR":
+                return calculaTestador();
+            default:
+                System.out.println("DIGITE O CARGO CORRETAMENTE");
+                return new BigDecimal(0);
         }
     }
-    private BigDecimal calculaDesenvolvedor(BigDecimal SALARIO_BASE){
-        this.salario = SALARIO_BASE;
-        return salario.multiply(DESCONTO_DE_25);
+
+    private BigDecimal calculaDesenvolvedor() {
+        if (salario.compareTo(SALARIO_BASE_DESENVOLVEDOR) >= 0) {            
+            return salario.subtract(salario.multiply(DESCONTO_DE_20));
+        } else {
+            return salario.subtract(salario.multiply(DESCONTO_DE_10));
+        }
+    }
+
+    private BigDecimal calculaGerente() {
+        if (salario.compareTo(SALARIO_BASE_GERENTE) >= 0) {
+            return salario.multiply(DESCONTO_DE_30);
+        } else {
+            return salario.multiply(DESCONTO_DE_20);
+        }
+    }
+
+    private BigDecimal calculaDBA() {
+        if (salario.compareTo(SALARIO_BASE_DBA) >= 0) {
+            return salario.subtract(salario.multiply(DESCONTO_DE_25));
+        } else {
+            return salario.subtract(salario.multiply(DESCONTO_DE_15));
+        }
+    }
+
+    private BigDecimal calculaTestador() {
+        if (salario.compareTo(SALARIO_BASE_TESTADOR) >= 0) {
+            return salario.subtract(salario.multiply(DESCONTO_DE_25));
+        } else {
+            return salario.subtract(salario.multiply(DESCONTO_DE_15));
+        }
     }
 }
--- test/br/edu/uricer/CalculaSalarioTest.java
+++ test/br/edu/uricer/CalculaSalarioTest.java
@@ -1,5 +1,6 @@
 package br.edu.uricer;
 
+import java.math.BigDecimal;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,8 +18,17 @@ public class CalculaSalarioTest {
     }
         
     @Test
-    public void testSomeMethod() {
-        
+    public void testSalarioDESENVOLVEDOR() {
+        CalculaSalario test = new CalculaSalario(new BigDecimal("5000.0"), "DESENVOLVEDOR");
+        BigDecimal esperado =  new BigDecimal("4000.00");
+        assertEquals(esperado,test.usaCargo());
+    }
+    
+    @Test
+    public void testSalarioDBA() {
+        CalculaSalario test = new CalculaSalario(new BigDecimal("550.0"), "TESTADOR");
+        BigDecimal esperado =  new BigDecimal("467.50");
+        assertEquals(esperado,test.usaCargo());
     }
     
 }
